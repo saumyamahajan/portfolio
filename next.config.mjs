3d7de6b46d0c05251749e3959c401f8549d7223c
saumyamahajan <saumyamahajan06@gmail.com>
2026-09-13 18:57:57 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
+  output: 'export',
+
+  basePath: isGithubPages ? '/portfolio' : '',
+  assetPrefix: isGithubPages ? '/portfolio/' : '',
+
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
